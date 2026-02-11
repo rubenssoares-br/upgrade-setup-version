3210f198508cf3636075d18c61a9bcfdb1819aa8
Rubens Soares <rubens.soares@liferay.com>
2026-02-11 15:30:16 -0300

    ECU-15056 Add --plugins-version parameter and integrate settings.gradle update step

--- src/main/java/com/liferay/upgrades/main/Main.java
+++ src/main/java/com/liferay/upgrades/main/Main.java
@@ -6,6 +6,7 @@ import com.beust.jcommander.ParameterException;
 import com.liferay.upgrades.project.dependency.docker.UpdateDockerCompose;
 import com.liferay.upgrades.project.dependency.git.GitHandler;
 import com.liferay.upgrades.project.dependency.gradle.UpdateGradleProperties;
+import com.liferay.upgrades.project.dependency.gradle.UpdateSettingsGradle;
 
 import java.util.logging.Logger;
 
@@ -44,6 +45,16 @@ public class Main {
                     gitHandler.commit(versionOptions.directory, commitMsgStep2);
                 }
 
+                _log.info("Step 3 Updating settings.gradle...");
+
+                UpdateSettingsGradle updateSettingsGradle = new UpdateSettingsGradle();
+
+                String oldPluginVersion = updateSettingsGradle.run(versionOptions.directory, versionOptions.pluginsVersion);
+
+                String commitMsgStep3 = String.format("%s Update com.liferay.gradle.plugins.workspace from %s to %s in settings.gradle", versionOptions.ticket, oldPluginVersion, versionOptions.pluginsVersion);
+
+                gitHandler.commit(versionOptions.directory, commitMsgStep3);
+
             }
         } catch (Exception  exception) {
             if (exception instanceof ParameterException) {
@@ -57,6 +68,7 @@ public class Main {
         return """
                 The available options are:
                 \t--ticket or -t to set the Jira ticket ID (Required)
+                \t--plugin-version or -p to set the new Liferay workspace plugin version
                 \t--liferay-version or -l to set the new Liferay upgrade version (Required)
                 \t--docker-compose or -d to set the new image liferay version in docker compose
                 \t--folder or -f to specify the path for the liferay workspace (Required)
@@ -83,6 +95,13 @@ public class Main {
                 required = true
         )
         String ticket;
+
+        @Parameter(
+                names = {"-p", "--plugin-version"},
+                description = "Set the new liferay workspace plugin version",
+                required = true
+        )
+        String pluginsVersion;
 
          @Parameter(
             names = {"-l", "--liferay-version"},
