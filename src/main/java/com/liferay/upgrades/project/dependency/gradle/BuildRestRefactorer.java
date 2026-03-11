2b7017aaf238e8477d39a8fe2bb5c2e21e9e1482
Rubens Soares <rubens.soares@liferay.com>
2026-03-11 08:48:20 -0300

    ECU-15588 Add BuildRestRefactorer to identify and run buildRest on REST modules

---
+++ src/main/java/com/liferay/upgrades/project/dependency/gradle/BuildRestRefactorer.java
@@ -0,0 +1,56 @@
+package com.liferay.upgrades.project.dependency.gradle;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class BuildRestRefactorer {
+
+    public List<File> findRestModules(String directory) {
+        List<File> restModules = new ArrayList<>();
+        File modulesDir = new File(directory, "modules");
+
+        if (modulesDir.exists() && modulesDir.isDirectory()) {
+            _findRestConfigYaml(modulesDir, restModules);
+        }
+
+        return restModules;
+    }
+
+    private void _findRestConfigYaml(File directory, List<File> restModules) {
+        File[] files = directory.listFiles();
+        if (files == null) return;
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                _findRestConfigYaml(file, restModules);
+            } else if (file.getName().equals("rest-config.yaml")) {
+                restModules.add(file.getParentFile());
+            }
+        }
+    }
+
+    public void run(String modulePath) throws Exception {
+        _log.info("Running buildRest in " + modulePath);
+        _executeShell(modulePath, "blade gw buildRest");
+    }
+
+    private void _executeShell(String directory, String command) throws Exception {
+        ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", command);
+
+        processBuilder.directory(new File(directory));
+
+        processBuilder.redirectErrorStream(true);
+
+        Process process = processBuilder.start();
+
+        int exitCode = process.waitFor();
+
+        if (exitCode != 0) {
+            _log.warning("Command '" + command + "' failed with exit code: " + exitCode);
+        }
+    }
+
+    private static final Logger _log = Logger.getLogger(BuildRestRefactorer.class.getName());
+}
